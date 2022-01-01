bc72812809bc20b1d1678054d3ab515b8281cc4e
CoToYo <56789038+CoToYo@users.noreply.github.com>
2022-01-02 02:20:48 +0800

    更新了请假申请接口

--- src/main/java/com/example/InternshipPlatform_1/InternshipPlatform_1/controller/LeaveController.java
+++ src/main/java/com/example/InternshipPlatform_1/InternshipPlatform_1/controller/LeaveController.java
@@ -13,7 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
 
 /**
  * <p>
@@ -35,13 +39,21 @@ public class LeaveController {
     @PostMapping(value = "/confirm", consumes = "application/json")
     public String uploadLeaveRecord(@RequestBody Leave leave){
 
+        System.out.println("LeaveTime: "+leave.getLeaveTime());
+
+        //数据格式转化: LocalDateTime->Date
+//        ZoneId zoneId = ZoneId.systemDefault();
+//        ZonedDateTime zdt = LocalDateTime.now().atZone(zoneId);
+//        Date present_time = Date.from(zdt.toInstant());
+
         LocalDateTime present_time = LocalDateTime.now();
         leave.setAskTime(present_time);//后台生成请假申请的请求时间
+
 
         User applicantUser = UserHolder.getUser();//当前操作者(请假申请者)的信息
         leave.setApplicantId(applicantUser.getUserId());//记录请假申请者的ID
         leave.setApplicantName(userMapper.selectById(applicantUser.getUserId()).getUserName());//记录请假申请者的姓名
-
+        System.out.println(leave);
 
         leaveMapper.insert(leave);//将请假信息写入数据库
 
