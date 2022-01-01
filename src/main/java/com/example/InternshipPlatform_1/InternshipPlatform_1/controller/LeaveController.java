0936a8ad2de26cd9a218d049ac3181d6312801a0
Kserpass <827401565@qq.com>
2022-01-02 02:07:03 +0800

    更新请假历史接口/leave/leaveHistory

--- src/main/java/com/example/InternshipPlatform_1/InternshipPlatform_1/controller/LeaveController.java
+++ src/main/java/com/example/InternshipPlatform_1/InternshipPlatform_1/controller/LeaveController.java
@@ -1,19 +1,16 @@
 package com.example.InternshipPlatform_1.InternshipPlatform_1.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.InternshipPlatform_1.InternshipPlatform_1.entity.Leave;
 import com.example.InternshipPlatform_1.InternshipPlatform_1.entity.User;
 import com.example.InternshipPlatform_1.InternshipPlatform_1.mapper.LeaveMapper;
-import com.example.InternshipPlatform_1.InternshipPlatform_1.mapper.UserMapper;
 import com.example.InternshipPlatform_1.InternshipPlatform_1.utils.UserHolder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * <p>
@@ -29,23 +26,29 @@ public class LeaveController {
 
     @Autowired
     LeaveMapper leaveMapper;
-    @Autowired
-    UserMapper userMapper;
 
     @PostMapping(value = "/confirm", consumes = "application/json")
     public String uploadLeaveRecord(@RequestBody Leave leave){
-
         LocalDateTime present_time = LocalDateTime.now();
         leave.setAskTime(present_time);//后台生成请假申请的请求时间
-
-        User applicantUser = UserHolder.getUser();//当前操作者(请假申请者)的信息
-        leave.setApplicantId(applicantUser.getUserId());//记录请假申请者的ID
-        leave.setApplicantName(userMapper.selectById(applicantUser.getUserId()).getUserName());//记录请假申请者的姓名
-
 
         leaveMapper.insert(leave);//将请假信息写入数据库
 
         return "The LeaveRecord has been uploaded successfully!";
+    }
+
+    @GetMapping(value = "/leaveHistory")
+    public List<Leave> leaveHistory() {
+        User applicant = UserHolder.getUser();
+        System.out.print(applicant);
+        System.out.println("Showing leave history:\n");
+
+//        leaveHistory = leaveMapper.getLeaveHistory(applicant.getUserId());
+        QueryWrapper<Leave> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("applicant_id",applicant.getUserId());
+        List<Leave> leaveHistory = leaveMapper.selectList(queryWrapper);
+        System.out.println(leaveHistory);
+        return leaveHistory;
     }
 
 }
